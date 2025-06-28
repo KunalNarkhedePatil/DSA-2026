7f166e5dc3cf91182e4ef9406914f8f8f461bfe5
KunalNarkhedePatil <narkhedekunal53@gmail.com>
2025-06-28 18:59:39 +0530

    commit

---
+++ ProblemsOnArrays/23_Non_Repeating_Element.cpp
@@ -0,0 +1,106 @@
+#include <iostream>
+#include <unordered_map>
+class Array
+{
+protected:
+    int iSize;
+    int* Arr;
+
+public:
+    Array(int iSize) : iSize(iSize), Arr(new int[iSize]) {}
+
+    virtual ~Array()
+    {
+        delete[] Arr;
+        Arr = nullptr;
+    }
+
+    Array(const Array& other)
+    {
+        this->iSize = other.iSize;
+        this->Arr = new int[iSize];
+        for (int i = 0; i < iSize; i++)
+        {
+            this->Arr[i] = other.Arr[i];
+        }
+    }
+
+    Array& operator=(const Array& other)
+    {
+        if (this == &other)
+            return *this;
+
+        delete[] this->Arr;
+        this->iSize = other.iSize;
+        this->Arr = new int[iSize];
+
+        for (int i = 0; i < iSize; i++)
+        {
+            this->Arr[i] = other.Arr[i];
+        }
+
+        return *this;
+    }
+
+    void Accept()
+    {
+        for (int i = 0; i < iSize; i++)
+        {
+            std::cin >> Arr[i];
+        }
+    }
+
+    void Display() const
+    {
+        for (int i = 0; i < iSize; i++)
+        {
+            std::cout << Arr[i] << " ";
+        }
+        std::cout << std::endl;
+    }
+};
+
+class ArrayDemo : public Array
+{
+public:
+    ArrayDemo(int iSize) : Array(iSize) {}
+
+    int firstNonRepeating() 
+    {
+        std::unordered_map<int,int> freq;
+
+        for(int i=0;i<iSize;i++)
+        {
+            freq[Arr[i]]++;
+        }
+
+        for(int i=0;i<iSize;i++)
+        {
+            if(freq[Arr[i]]==1)
+            {
+                return Arr[i];
+            }
+        }
+        return 0;
+    }
+};
+
+int main()
+{
+    int iSize;
+    std::cout << "Enter size of array: ";
+    std::cin >> iSize;
+
+    ArrayDemo obj(iSize);
+
+    std::cout << "Enter elements:\n";
+    obj.Accept();
+
+    std::cout << "Array elements:\n";
+    obj.Display();
+
+    int iRet=obj.firstNonRepeating();
+
+    std::cout<<"first non repeating  element is :"<<iRet<<std::endl;
+    return 0;
+}
