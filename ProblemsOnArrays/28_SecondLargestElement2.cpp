8cc7819147a20d7a2ecd13ae26e7f4b566b494cc
KunalNarkhedePatil <narkhedekunal53@gmail.com>
2025-06-29 17:27:30 +0530

    commmit

---
+++ ProblemsOnArrays/28_SecondLargestElement2.cpp
@@ -0,0 +1,117 @@
+#include <iostream>
+
+class Array
+{
+protected:
+    int iSize;
+    int* Arr;
+
+public:
+    Array(int iSize=5) : iSize(iSize), Arr(new int[iSize]) {}
+
+    virtual ~Array()
+    {
+        delete[] Arr;
+        Arr = nullptr;
+    }
+
+    Array(const Array& other)
+    {
+        this->iSize = other.iSize;
+        this->Arr = new int[iSize];
+        for (int i = 0; i < iSize; i++)
+        {
+            this->Arr[i] = other.Arr[i];
+        }
+    }
+
+    Array& operator=(const Array& other)
+    {
+        if (this == &other)
+            return *this;
+
+        delete[] this->Arr;
+        this->iSize = other.iSize;
+        this->Arr = new int[iSize];
+
+        for (int i = 0; i < iSize; i++)
+        {
+            this->Arr[i] = other.Arr[i];
+        }
+
+        return *this;
+    }
+
+    void Accept()
+    {
+        for (int i = 0; i < iSize; i++)
+        {
+            std::cin >> Arr[i];
+        }
+    }
+
+    void Display() const
+    {
+        for (int i = 0; i < iSize; i++)
+        {
+            std::cout << Arr[i] << " ";
+        }
+        std::cout << std::endl;
+    }
+};
+
+class ArrayDemo : public Array
+{
+public:
+    ArrayDemo(int iSize) : Array(iSize) {}
+
+    int FindLargest()
+    {
+        int iMax=Arr[0];
+        for(int i=0;i<iSize;i++)
+        {
+            if(Arr[i]>iMax)
+            {
+                iMax=Arr[i];
+            }
+        }
+        return iMax;
+    }
+
+    //1 2 3 4 5 5
+    int findSecondLargest()
+    {
+        int iMax=FindLargest();
+
+        std::cout<<"Max:"<<iMax<<std::endl;
+
+        for(int i=iSize-2;i>=0;i--)
+        {
+            if(iMax != Arr[i]) 
+            {
+                return Arr[i];
+            }
+        }
+    }
+      
+};
+
+int main()
+{
+    int iSize;
+    std::cout << "Enter size of array: ";
+    std::cin >> iSize;
+
+    ArrayDemo obj(iSize);
+
+    std::cout << "Enter elements:\n";
+    obj.Accept();
+
+    std::cout << "Array elements:\n";
+    obj.Display();
+
+    int iRet=obj.findSecondLargest();
+
+    std::cout<<"Second largest element is :"<<iRet<<std::endl;
+    return 0;
+}
--- ProblemsOnArrays/practice.cpp
+++ ProblemsOnArrays/practice.cpp
@@ -1,17 +1,87 @@
 #include<iostream>
 using namespace std;
 
-int main()
+//typedef Node* PNODE;
+class Node
 {
-    int iNo=153;
+    //friend class SinglyLL;
+    public:
+    int data;
+    Node* next;
 
-    int iCnt=0;
-    while(iNo>=9)
+    public:
+
+    Node(int val)
     {
-        iCnt++;
-        iNo/=10;
+        this->data=val;
+        this->next=NULL;
+    }
+};
+
+class SinglyLL
+{
+    private:
+
+    Node* first;
+    int iSize;
+public:
+    SinglyLL()
+    {
+        cout<<"Inside SinglyLL::SinglyLL()"<<endl;
+        this->first=NULL;
+        this->iSize=0;
     }
 
-    cout<<"Number of digit in iNo:"<<iCnt<<endl;
-    return (0);
+    void insertAsSorted(int iVal)
+    {
+        cout<<"Inside insertAsSorted:"<<endl;
+        Node* newn=new Node(iVal);
+
+        if(first==NULL || first->data>=iVal)
+        {
+            std::cout<<"Inside first"<<std::endl;
+            newn->next=first;
+            first=newn;
+        }
+        else
+        {
+            std::cout<<"Inside second"<<std::endl;
+            Node* temp=first;
+            while(temp->next!=NULL && temp->next->data<=iVal)
+            {
+                temp=temp->next;
+            }
+
+            newn->next=temp->next;
+            temp->next=newn;
+        }
+        iSize++;
+    }
+    void Display()
+    {
+        std::cout<<"Inside Display():"<<std::endl;
+        Node* temp=first;
+
+        while(temp!=NULL)
+        {
+            std::cout<<temp->data<<" ";
+            temp=temp->next;
+        }
+        std::cout<<std::endl;
+    }
+};
+
+int main()
+{
+    std::cout<<"Inside Main...."<<std::endl;
+    SinglyLL sobj;
+
+    sobj.insertAsSorted(10);
+    sobj.insertAsSorted(5);
+    sobj.insertAsSorted(15);
+    sobj.insertAsSorted(30);
+    sobj.insertAsSorted(20);
+
+    sobj.Display();
+    return 0;
 }
