8bb306752de175ed2a46a9dec2a30790b637e4fb
KunalNarkhedePatil <narkhedekunal53@gmail.com>
2025-06-29 23:31:05 +0530

    commit

---
+++ ProblemsOnArrays/33_checkArraySorted.cpp
@@ -0,0 +1,114 @@
+#include <iostream>
+#include<algorithm>
+#include<climits>
+class Array
+{
+protected:
+    int iSize;
+    int *Arr;
+
+public:
+    Array(int iSize = 5) : iSize(iSize), Arr(new int[iSize]) {}
+
+    virtual ~Array()
+    {
+        delete[] Arr;
+        Arr = nullptr;
+    }
+
+    Array(const Array &other)
+    {
+        this->iSize = other.iSize;
+        this->Arr = new int[iSize];
+        for (int i = 0; i < iSize; i++)
+        {
+            this->Arr[i] = other.Arr[i];
+        }
+    }
+
+    Array &operator=(const Array &other)
+    {
+        if (this == &other)
+            return *this;
+
+        delete[] this->Arr;
+        this->iSize = other.iSize;
+        this->Arr = new int[iSize];
+
+        for (int i = 0; i < iSize; i++)
+        {
+            this->Arr[i] = other.Arr[i];
+        }
+
+        return *this;
+    }
+
+    void Accept()
+    {
+        for (int i = 0; i < iSize; i++)
+        {
+            std::cin >> Arr[i];
+        }
+    }
+
+    void Display() const
+    {
+        for (int i = 0; i < iSize; i++)
+        {
+            std::cout << Arr[i] << " ";
+        }
+        std::cout << std::endl;
+    }
+};
+
+class ArrayDemo : public Array
+{
+public:
+    ArrayDemo(int iSize) : Array(iSize) {}
+
+   
+   bool checkArraySorted()
+   {
+    
+       for(int i=1;i<iSize;i++)
+       {
+          if(Arr[i]>=Arr[i-1])
+          {
+              continue;
+          }
+          else 
+          {
+              return false;
+          }
+       }
+
+      return true;
+   }
+};
+
+int main()
+{
+    int iSize;
+    std::cout << "Enter size of array: ";
+    std::cin >> iSize;
+
+    ArrayDemo obj(iSize);
+
+    std::cout << "Enter elements:\n";
+    obj.Accept();
+
+    std::cout << "Array elements:\n";
+    obj.Display();
+
+    bool bRet = obj.checkArraySorted();
+
+    if(bRet==true)
+    {
+        std::cout<<"Given an array is sorted"<<std::endl;
+    }
+    else
+    {
+        std::cout<<"Given an array is not sorted"<<std::endl;
+    }
+    return 0;
+}
