5be9d5b30fc9e105fcb20e6f948b2a849279557a
KunalNarkhedePatil <narkhedekunal53@gmail.com>
2025-06-29 23:54:02 +0530

    commit

---
+++ ProblemsOnArrays/34_removeDuplicateFromSortedArray.cpp
@@ -0,0 +1,126 @@
+//https://leetcode.com/problems/remove-duplicates-from-sorted-array/submissions/1680658450/
+#include <iostream>
+#include<set>
+class Array
+{
+protected:
+    int iSize;
+    int *Arr;
+
+public:
+    Array(int iSize = 5) : iSize(iSize), Arr(new int[iSize]) {}
+
+    virtual ~Array()
+    {
+        delete[] Arr;
+        Arr = nullptr;
+    }
+
+    Array(const Array &other)
+    {
+        this->iSize = other.iSize;
+        this->Arr = new int[iSize];
+        for (int i = 0; i < iSize; i++)
+        {
+            this->Arr[i] = other.Arr[i];
+        }
+    }
+
+    Array &operator=(const Array &other)
+    {
+        if (this == &other)
+            return *this;
+
+        delete[] this->Arr;
+        this->iSize = other.iSize;
+        this->Arr = new int[iSize];
+
+        for (int i = 0; i < iSize; i++)
+        {
+            this->Arr[i] = other.Arr[i];
+        }
+
+        return *this;
+    }
+
+    void Accept()
+    {
+        for (int i = 0; i < iSize; i++)
+        {
+            std::cin >> Arr[i];
+        }
+    }
+
+    void Display() const
+    {
+        for (int i = 0; i < iSize; i++)
+        {
+            std::cout << Arr[i] << " ";
+        }
+        std::cout << std::endl;
+    }
+};
+
+class ArrayDemo : public Array
+{
+public:
+    ArrayDemo(int iSize) : Array(iSize) {}
+
+   
+   int removeDuplicateFromSortedArray()
+   {
+       std::set<int> s;
+
+       for(int i=0;i<iSize;i++)
+       {
+           s.insert(Arr[i]);
+       }
+
+       int index=0;
+
+       std::cout<<"Size of Set:"<<s.size()<<std::endl;
+
+       for(auto it:s)
+       {
+           Arr[index]=it;
+           index++;
+       }
+
+       return s.size();
+   }
+
+   void Display(int size)
+   {
+      for(int i=0;i<size;i++)
+      {
+          std::cout<<Arr[i]<<" ";
+      }
+      std::cout<<std::endl;
+   }
+
+
+};
+
+int main()
+{
+    int iSize;
+    std::cout << "Enter size of array: ";
+    std::cin >> iSize;
+
+    Array *pObj=new ArrayDemo(iSize);
+
+    std::cout << "Enter elements:\n";
+    pObj->Accept();
+
+    std::cout << "Array elements:\n";
+    pObj->Display();
+
+    int iRet = reinterpret_cast<ArrayDemo*>(pObj)->removeDuplicateFromSortedArray();
+
+    std::cout<<"iRet:"<<iRet<<std::endl;
+
+    std::cout<<"Check After remove duplicate unique element:"<<std::endl;
+
+    reinterpret_cast<ArrayDemo*>(pObj)->Display(iRet);
+    return 0;
+}
