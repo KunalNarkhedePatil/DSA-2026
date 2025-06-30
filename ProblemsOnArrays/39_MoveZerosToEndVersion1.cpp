6ed0d5eb9e4bfafe8a729e10ccdb7d692488cf98
KunalNarkhedePatil <narkhedekunal53@gmail.com>
2025-07-01 05:26:39 +0530

    commmit

---
+++ ProblemsOnArrays/39_MoveZerosToEndVersion1.cpp
@@ -0,0 +1,113 @@
+#include <iostream>
+#include<vector>
+class Array
+{
+protected:
+    int iSize;
+    int* Arr;
+
+public:
+    Array(int iSize) : iSize(iSize), Arr(new int[iSize]) {}
+
+    virtual ~Array()
+    {
+        delete[] Arr;
+        Arr = nullptr;
+    }
+
+    Array(const Array& other)
+    {
+        this->iSize = other.iSize;
+        this->Arr = new int[iSize];
+        for (int i = 0; i < iSize; i++)
+        {
+            this->Arr[i] = other.Arr[i];
+        }
+    }
+
+    Array& operator=(const Array& other)
+    {
+        if (this == &other)
+            return *this;
+
+        delete[] this->Arr;
+        this->iSize = other.iSize;
+        this->Arr = new int[iSize];
+
+        for (int i = 0; i < iSize; i++)
+        {
+            this->Arr[i] = other.Arr[i];
+        }
+
+        return *this;
+    }
+
+    void Accept()
+    {
+        for (int i = 0; i < iSize; i++)
+        {
+            std::cin >> Arr[i];
+        }
+    }
+
+    void Display() const
+    {
+        for (int i = 0; i < iSize; i++)
+        {
+            std::cout << Arr[i] << " ";
+        }
+        std::cout << std::endl;
+    }
+};
+
+class ArrayDemo : public Array
+{
+public:
+    ArrayDemo(int iSize) : Array(iSize) {}
+
+    void moveZeroAtEnd()
+    {
+        std::vector<int> temp;
+
+        for(int i=0;i<iSize;i++)
+        {
+            if(Arr[i]!=0)
+            {
+                temp.push_back(Arr[i]);
+            }
+        }
+
+        for(int i=0;i<temp.size();i++)
+        {
+            Arr[i]=temp[i];
+        }
+        for(int i=temp.size();i<iSize;i++)
+        {
+            Arr[i]=0;
+        }
+    }
+};
+
+int main()
+{
+    int iSize;
+    std::cout << "Enter size of array: ";
+    std::cin >> iSize;
+
+    ArrayDemo obj(iSize);
+
+    std::cout << "Enter elements:\n";
+    obj.Accept();
+
+    std::cout << "Array elements:\n";
+    obj.Display();
+
+    obj.moveZeroAtEnd();
+
+    std::cout<<"After Moving Zero At the End Array is :"<<std::endl;
+
+    obj.Display();
+
+    
+    return 0;
+}
---
+++ ProblemsOnArrays/40_MoveZerosToEndVersion2.cpp
@@ -0,0 +1,125 @@
+#include <iostream>
+#include<vector>
+class Array
+{
+protected:
+    int iSize;
+    int* Arr;
+
+public:
+    Array(int iSize) : iSize(iSize), Arr(new int[iSize]) {}
+
+    virtual ~Array()
+    {
+        delete[] Arr;
+        Arr = nullptr;
+    }
+
+    Array(const Array& other)
+    {
+        this->iSize = other.iSize;
+        this->Arr = new int[iSize];
+        for (int i = 0; i < iSize; i++)
+        {
+            this->Arr[i] = other.Arr[i];
+        }
+    }
+
+    Array& operator=(const Array& other)
+    {
+        if (this == &other)
+            return *this;
+
+        delete[] this->Arr;
+        this->iSize = other.iSize;
+        this->Arr = new int[iSize];
+
+        for (int i = 0; i < iSize; i++)
+        {
+            this->Arr[i] = other.Arr[i];
+        }
+
+        return *this;
+    }
+
+    void Accept()
+    {
+        for (int i = 0; i < iSize; i++)
+        {
+            std::cin >> Arr[i];
+        }
+    }
+
+    void Display() const
+    {
+        for (int i = 0; i < iSize; i++)
+        {
+            std::cout << Arr[i] << " ";
+        }
+        std::cout << std::endl;
+    }
+};
+
+class ArrayDemo : public Array
+{
+public:
+    ArrayDemo(int iSize) : Array(iSize) {}
+
+    void swap(int *p,int *q)
+    {
+        int temp=*p;
+        *p=*q;
+        *q=temp;
+    }
+    void moveZeroAtEnd()
+    {
+        int j=-1;
+
+        for(int i=0;i<iSize;i++)
+        {
+            if(Arr[i]==0)
+            {
+                j=i;
+                break;
+            }
+        }
+
+        if(j==-1)
+        {
+           return;
+        }
+
+        for(int i=j+1;i<iSize;i++)
+        {
+            if(Arr[i]!=0)
+            {
+                swap(&Arr[i],&Arr[j]);
+                j++;
+            }
+        }
+    }
+};
+
+int main()
+{
+    int iSize;
+    std::cout << "Enter size of array: ";
+    std::cin >> iSize;
+
+    ArrayDemo obj(iSize);
+
+    std::cout << "Enter elements:\n";
+    obj.Accept();
+
+    std::cout << "Array elements:\n";
+    obj.Display();
+
+    obj.moveZeroAtEnd();
+
+    std::cout<<"After Moving Zero At the End Array is :"<<std::endl;
+
+    obj.Display();
+
+    
+    return 0;
+}
