a44e5f9bfd90816ebfc8ab1b8f0e5d3da6fcc431
KunalNarkhedePatil <narkhedekunal53@gmail.com>
2025-07-02 04:40:18 +0530

    commit

---
+++ ProblemsOnArrays/46_FindElementAppearsOnce(OptimalSolution).cpp
@@ -0,0 +1,98 @@
+#include <iostream>
+#include <unordered_map>
+class Array
+{
+protected:
+    int iSize;
+    int *Arr;
+
+public:
+    Array(int iSize = 5) : iSize(iSize), Arr(new int[iSize]) {}
+
+    virtual ~Array()
+    {
+        delete[] Arr;
+        Arr = nullptr;
+    }
+
+    Array(const Array &other)
+    {
+        this->iSize = other.iSize;
+        this->Arr = new int[iSize];
+        for (int i = 0; i < iSize; i++)
+        {
+            this->Arr[i] = other.Arr[i];
+        }
+    }
+
+    Array &operator=(const Array &other)
+    {
+        if (this == &other)
+            return *this;
+
+        delete[] this->Arr;
+        this->iSize = other.iSize;
+        this->Arr = new int[iSize];
+
+        for (int i = 0; i < iSize; i++)
+        {
+            this->Arr[i] = other.Arr[i];
+        }
+
+        return *this;
+    }
+
+    void Accept()
+    {
+        for (int i = 0; i < iSize; i++)
+        {
+            std::cin >> Arr[i];
+        }
+    }
+
+    void Display() const
+    {
+        for (int i = 0; i < iSize; i++)
+        {
+            std::cout << Arr[i] << " ";
+        }
+        std::cout << std::endl;
+    }
+};
+
+class ArrayDemo : public Array
+{
+public:
+    ArrayDemo(int iSize) : Array(iSize) {}
+
+    int findElementAppearsOnce()
+    {
+       int XOR=0;
+
+       for(int i=0;i<iSize;i++)
+       {
+           XOR=XOR^Arr[i];
+       }
+       return XOR;
+    }
+};
+
+int main()
+{
+    int iSize;
+    std::cout << "Enter size of array: ";
+    std::cin >> iSize;
+
+    ArrayDemo obj(iSize);
+
+    std::cout << "Enter elements:\n";
+    obj.Accept();
+
+    std::cout << "Array elements:\n";
+    obj.Display();
+
+    int iRet = obj.findElementAppearsOnce();
+
+    std::cout << "Number Which is occurs only once:" << iRet << std::endl;
+    return 0;
+}
